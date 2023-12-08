7ee811da7a1e2ae841826130693e2f76d7bc8221
Davrbek Mirzakosimov <112061049+davrbek-economist@users.noreply.github.com>
2023-12-08 07:31:33 +0100

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+#Title
+st.title('Transport classifying model')
+
+#Uploading
+file = st.file_uploader("Uploading image", type=['png','jpeg','gif','svg'])
+if file:
+    st.image(file)
+    #PIL convert
+    img = PILImage.create(file)
+
+    #model
+    model = load_learner('transport_model.pkl')
+
+    # prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Prediction: {pred}")
+    st.info(f'Probability: {probs[pred_id]*100:.1f}%')
+    #plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
